c4c542d5f72129635b48b0248c03eba19f1fb77a
Tomas Kristiansson <tomas.kson@gmail.com>
2020-12-31 16:57:51 +0100

    powershell makefile

---
+++ makefileish.ps1
@@ -0,0 +1,43 @@
+#because i have no idea what i'm doing, i rewrote parts of Makefile to powershell
+
+[CmdletBinding()]
+param(
+	[parameter(Mandatory, Position = 0)]
+	[ValidateSet('All', 'Build', 'Run', 'Clean')]
+	[string[]]$Action
+)
+
+if ('All' -eq $Action) {
+	$Action = 'Clean', 'Build', 'Run'
+}
+
+$SRC = @(
+	'src/CoordCube.java'
+	'src/CubieCube.java'
+	'src/Search.java'
+	'src/Util.java'
+	'src/Tools.java'
+)
+$MAINPROG = 'example/MainProgram.java'
+$TESTSRC  = 'test/test.java'
+$DIST     = 'dist/twophase.jar'
+$DISTTEST = 'dist/test.class'
+
+switch ($Action) {
+	'Build' {
+		Write-Host "* $_"
+		javac.exe -d 'dist' $SRC $MAINPROG -Xlint:all
+		Copy-Item -Force -Path $SRC -Destination 'dist/cs/min2phase'
+		cd 'dist'
+		jar.exe cfe 'twophase.jar' 'ui.MainProgram' 'ui/*.class' 'cs/min2phase/*.class' 'cs/min2phase/*.java'
+		cd ..
+	}
+	'Run' {
+		Write-Host "* $_"
+		java.exe -jar $DIST
+	}
+	'Clean' {
+		Write-Host "* $_"
+		Remove-Item -Recurse dist -ErrorAction SilentlyContinue
+	}
+}
